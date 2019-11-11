b02ce1945c0dbe860105bbd83475e7bf3c2f916a
Muhammad Mahin Dar <muhammad@rocketbots.io>
2019-11-12 01:00:21 +0500

    Comment changed

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,4 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-//I just commented from my computer
+//Hello this is the power of git
